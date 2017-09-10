e83df0a06e78fbe4ce48ce328740f827cf8553cc
Prateek Chaubey <chaubeyprateek@gmail.com>
2017-09-10 14:01:30 +0000

    jalebi: add ims props

---
+++ product/ims.mk
@@ -0,0 +1,7 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.jbims=1 \
+    persist.radio.ROTATION_ENABLE=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.VT_USE_MDM_TIME=0 \
+    service.qti.ims.enabled=1
