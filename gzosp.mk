85fe324ae7fe1754c68efe98900ea139230bedde
iamsj7 <shaikjaleel270@gmail.com>
2017-09-19 22:51:45 +0530

    temp

--- gzosp.mk
+++ gzosp.mk
@@ -16,12 +16,12 @@
 $(call inherit-product, device/yu/jalebi/full_jalebi.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-PRODUCT_NAME := lineage_jalebi
+PRODUCT_NAME := gzosp_jalebi
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := jalebi
 
